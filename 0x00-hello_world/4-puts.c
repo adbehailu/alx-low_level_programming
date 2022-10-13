4abafd1212c20e044985ee456b33fd5a123533d1
adbehailu <adbehailu@gmail.com>
2022-10-13 14:30:08 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * Descroption: main - prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
