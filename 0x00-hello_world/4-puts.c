522e989a20f23298630b7ddb849cfaf396f10394
adbehailu <adbehailu@gmail.com>
2022-10-13 12:28:08 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
-/**
+/*
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
@@ -9,4 +8,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
