8e3d711e32e1e38e937b4375cfe5c6c8d45e742a
adbehailu <adbehailu@gmail.com>
2022-10-26 13:27:13 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,11 +2,13 @@
 
 /**
  * main - Entry point
- * decription: 'the program description
+ * Description: 'the program description
  * Return: Always 0 (Success)
- * write your line of code here...
+ * write your line of code here ...
  * Remember:
- * - you are not allowed to use a
+ * you are not allowed to use a
+ * you are not allowed to modify p
+ * you are not allowed to code anything else than this line of code
  */
 
 int main(void)
@@ -17,6 +19,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	*(p + 5) = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);
